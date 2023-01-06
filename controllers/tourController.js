cf19617ded8537c7a99e65ba9e75a960867b3cff
AndresUribe1234 <anduri1997@gmail.com>
2023-01-06 12:35:57 -0500

    code cleaned without db functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,129 +3,42 @@ const Tour = require(`${__dirname}/../models/tourModel.js`);
 
 const app = require(`${__dirname}/../app.js`);
 
-// Getting info from json file instead of database for testing purposes. After db connection is useless.
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/tours.json`)
-// );
-
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
-    length: Object.keys(tours).length,
-    data: { tours },
+    // length: Object.keys(tours).length,
+    // data: { tours },
   });
 };
 
 exports.createNewTour = (req, res) => {
-  const newId = tours[tours.length - 1]._id + 1;
-  const newTour = Object.assign({ _id: newId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/tours.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: "success",
-        length: Object.keys(tours).length,
-        data: { tours },
-      });
-      console.log(err);
-    }
-  );
+  res.status(200).json({
+    status: "success",
+    // length: Object.keys(tours).length,
+    // data: { tours },
+  });
 };
 
 exports.getTourById = (req, res) => {
-  const tourId = tours.find((ele) => {
-    if (!+req.params.id) {
-      return ele._id === req.params.id;
-    } else {
-      return ele._id === +req.params.id;
-    }
+  res.status(200).json({
+    status: "success",
+    // length: Object.keys(tours).length,
+    // data: { tours },
   });
-
-  if (tourId) {
-    res.status(200).json({
-      status: "success",
-      length: 1,
-      data: { tourId },
-    });
-  } else {
-    res.status(404).json({
-      status: "id does not exist",
-    });
-  }
 };
 
 exports.updateTourById = (req, res) => {
-  const tourId = tours.find((ele) => {
-    if (!+req.params.id) {
-      return ele._id === req.params.id;
-    } else {
-      return ele._id === +req.params.id;
-    }
+  res.status(200).json({
+    status: "success",
+    // length: Object.keys(tours).length,
+    // data: { tours },
   });
-
-  const tourArrayId = tours.map((ele) => ele._id);
-  const positionId = tourArrayId.indexOf(
-    !+req.params.id ? req.params.id : +req.params.id
-  );
-
-  if (tourId) {
-    Object.keys(req.body).forEach((ele, ind) => {
-      tours[positionId][`${ele}`] = req.body[`${ele}`];
-    });
-
-    fs.writeFile(
-      `${__dirname}/dev-data/tours.json`,
-      JSON.stringify(tours),
-      (err) => {
-        res.status(200).json({
-          status: "success",
-          length: 1,
-          data: tours[positionId],
-        });
-        console.log(err);
-      }
-    );
-  } else {
-    res.status(404).json({
-      status: "id does not exist",
-    });
-  }
 };
 
 exports.deleteTourById = (req, res) => {
-  const tourId = tours.find((ele) => {
-    if (!+req.params.id) {
-      return ele._id === req.params.id;
-    } else {
-      return ele._id === +req.params.id;
-    }
+  res.status(200).json({
+    status: "success",
+    // length: Object.keys(tours).length,
+    // data: { tours },
   });
-
-  const tourArrayId = tours.map((ele) => ele._id);
-  const positionId = tourArrayId.indexOf(
-    !+req.params.id ? req.params.id : +req.params.id
-  );
-
-  if (tourId) {
-    const elementRemoved = tours.splice(positionId, 1);
-    fs.writeFile(
-      `${__dirname}/dev-data/tours.json`,
-      JSON.stringify(tours),
-      (err) => {
-        res.status(200).json({
-          status: "success",
-          length: 1,
-          elementRemoved: elementRemoved,
-        });
-        console.log(err);
-      }
-    );
-  } else {
-    res.status(404).json({
-      status: "id does not exist",
-    });
-  }
 };
--- routes/tours.js
+++ routes/tours.js
@@ -4,10 +4,15 @@ const tourController = require(`${__dirname}/../controllers/tourController.js`);
 
 const router = express.Router();
 
-router.get("/", tourController.getAllTours);
-router.post("/", tourController.createNewTour);
-router.get("/:id", tourController.getTourById);
-router.patch("/:id", tourController.updateTourById);
-router.delete("/:id", tourController.deleteTourById);
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(tourController.createNewTour);
+
+router
+  .route("/:id")
+  .get(tourController.getTourById)
+  .patch(tourController.updateTourById)
+  .delete(tourController.deleteTourById);
 
 module.exports = router;
